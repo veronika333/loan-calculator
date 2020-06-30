e6dae600ccd3b0f6069b610d926c44cc61eadf0e
veronika333 <vera3@inbox.ru>
2020-06-30 13:24:27 +0300

    Message component added, conditional rendering to display message in app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './skeleton.css';
 import Form from './components/Form';
 import {calculateTotal} from './helper';
 import Result from './components/Result';
+import Message from './components/Message';
 
 class App extends Component {
   state = {
@@ -21,13 +22,20 @@ class App extends Component {
   }
   render() {
     const { amount, term, total } = this.state;
+// conditional rendering
+let component;
+if (total === '') {
+component = <Message />
+} else {
+component = <Result amount={amount} term={term} total={total} />
+}
     return (
       <Fragment>
       <h1>Loan Calculator</h1>
       <div className="container">
       <Form loanInformation={this.loanInformation} />
       <div className="messages">
-      <Result amount={amount} term={term} total={total} />
+      {component}
       </div>
       </div>
       
